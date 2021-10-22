75fca5455fa7970600bf56fd1e897669b6511c49
asdas <gercio1234567890@gmail.com>
2021-10-22 19:07:31 +0200

    abc

--- app/src/main/java/pollub/ism/myfrags/MainActivity.java
+++ app/src/main/java/pollub/ism/myfrags/MainActivity.java
@@ -1,37 +1,119 @@
 package pollub.ism.myfrags;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.fragment.app.Fragment;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity{
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-    private FragmentManager fragmentManager;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
-    private Fragment fragment1, fragment2, fragment3, fragment4;
+public class MainActivity extends FragmentActivity implements Fragment1.OnButtonClickListener {
+
+    private int[] frames;
+    private boolean hiden;
+
+    private int [] sequence; // <--- Nowe pole. Opisuje sekwencje fragmentów.
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fragment1 = new Fragment1();
-        fragment2 = new Fragment2();
-        fragment3 = new Fragment3();
-        fragment4 = new Fragment4();
+        if (savedInstanceState == null) {
+            frames = new int[]{R.id.frame1, R.id.frame2, R.id.frame3, R.id.frame4};
+            hiden = false;
 
-        fragmentManager = getSupportFragmentManager();
+            sequence = new int[]{0,1,2,3}; // <--- Początkowa sekwencja fragmentów
 
+            Fragment[] fragments = new Fragment[]{new Fragment1(), new Fragment2(), new Fragment3(), new Fragment4()};
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            for (int i = 0; i < 4; i++) {
+                transaction.add(frames[i], fragments[i]);
+            }
+            transaction.addToBackStack(null);
+            transaction.commit();
+        } else {
+            frames = savedInstanceState.getIntArray("FRAMES");
+            hiden = savedInstanceState.getBoolean("HIDEN");
+
+            sequence = savedInstanceState.getIntArray("SEQUENCE"); // <--- Nowy kod
+        }
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putIntArray("FRAMES", frames);
+        outState.putBoolean("HIDEN", hiden);
+
+        outState.putIntArray("SEQUENCE", sequence); // <--- Nowy kod
+    }
+
+
+    private void newFragments() {
+
+        Fragment[] newFragments = new Fragment[]{new Fragment1(), new Fragment2(), new Fragment3(), new Fragment4()};
+
+        //--- Nowy kod
+        Fragment[] inSequence = new Fragment[] {newFragments[sequence[0]], newFragments[sequence[1]], newFragments[sequence[2]], newFragments[sequence[3]] };
+        newFragments = inSequence;
+        //--- Koniec nowego kodu
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.add(R.id.frame1, fragment1);
-        transaction.add(R.id.frame2, fragment2);
-        transaction.add(R.id.frame3, fragment3);
-        transaction.add(R.id.frame4, fragment4);
+
+        for (int i = 0; i < 4; i++) {
+            transaction.replace(frames[i], newFragments[i]);
+            if (hiden && !(newFragments[i] instanceof Fragment1)) transaction.hide(newFragments[i]);
+        }
+
         transaction.addToBackStack(null);
         transaction.commit();
     }
 
+
+    @Override
+    public void onButtonClickHide() {
+        // Bez zmian
+    }
+
+
+    @Override
+    public void onButtonClickRestore() {
+        // Bez zmian
+    }
+
+
+    @Override
+    public void onButtonClickClockwise() {
+        // Bez zmian
+    }
+
+
+    @Override
+    public void onButtonClickShuffle() {
+
+        List<Integer> s = new ArrayList<>(Arrays.asList(sequence[0], sequence[1], sequence[2], sequence[3]));
+        Collections.shuffle(s);
+        for(int i = 0; i < 4; i++) sequence[i] = s.get(i);
+        //--- Koniec nowego kodu
+
+        newFragments();
+    }
+
+
+    @Override
+    public void onAttachFragment(@NonNull Fragment fragment) {
+        // Bez zmian
+    }
 }
